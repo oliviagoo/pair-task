7c76e67bfc856dbd6d1302345716cd67eea2ce61
Olivia Goodman <oliviagoodman@Olivias-MacBook-Air.local>
2020-03-02 09:43:50 +1300

    Update ex_9.py
    cleaning up and commenting

--- ex_9.py
+++ ex_9.py
@@ -2,28 +2,31 @@
 #28/02
 #ex 9
 
-#list
+#declaring constant
 TIMES = [["Crofton Downs", 7],["Ngaio", 2],["Awarua St", 2],["Simla Cres", 2],["Box Hill", 1],["Kandallah", 2],["Raroa", 3],["Johnsonville", 2]]
-#arrival_times = []
+
+#the function that calculates and outputs the timetable
 def time_calculation(i):
     print("-----------------")
+    #splitting the time into hours and minutes
     hour = i // 1
     minute = i % 1 * 100
-    print(minute)
+    #adding the departure time to the start of the list
     TIMES.insert(0, ["Wellington", 0])
-    #print(TIMES[0][1])
+    #adding and outputting times
     for time in TIMES:
-        #print(time)
         minute += time[1]
+        #rolling over to the next hour
         if minute >= 60:
             hour += 1
             minute -= 60
-        #arrival_times.append(minute)
+        #making sure the output has the right amount of digits
         if minute < 9:
             print("{}  {:.0f}:0{:.0f}".format(time[0], hour, minute), end = ". ")
         else:
             print("{}  {:.0f}:{:.0f}".format(time[0], hour, minute), end = ". ")
 
 #main routine
+#getting input - depart. time
 time_calculation(float(input("What time do you want to depart Wellington (in 24 hour time e.g. 13.01)? ")))
 
