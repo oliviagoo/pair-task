19ed2bf55596e4aef07df9a2b0a9b5cbfce59e7a
Olivia Goodman <oliviagoodman@Olivias-MacBook-Air.local>
2020-03-02 09:34:08 +1300

    Update ex_9.py
    one line input

--- ex_9.py
+++ ex_9.py
@@ -6,24 +6,22 @@
 TIMES = [["Crofton Downs", 7],["Ngaio", 2],["Awarua St", 2],["Simla Cres", 2],["Box Hill", 1],["Kandallah", 2],["Raroa", 3],["Johnsonville", 2]]
 arrival_times = []
 def time_calculation(i):
+    print("-----------------")
     hour = i // 1
     minute = i % 1 * 100
+    TIMES.insert(0, ["Wellington", minute])
     for time in TIMES:
-        print(time)
+        #print(time)
         minute += time[1]
         if minute >= 60:
             hour += 1
             minute -= 60
         arrival_times.append(minute)
         if minute < 9:
-            print("{:.0f}:0{:.0f}".format(hour, minute))
+            print("{}  {:.0f}:0{:.0f}".format(time[0], hour, minute), end = ". ")
         else:
-            print("{:.0f}:{:.0f}".format(hour, minute))
-
-def output_times():
-    for i in len(TIMES):
-        print(TIMES[i][0])
-        print("{}:{}".format(arrival_times[i]))
+            print("{}  {:.0f}:{:.0f}".format(time[0], hour, minute), end = ". ")
 
 #main routine
 time_calculation(float(input("What time do you want to depart Wellington (in 24 hour time e.g. 13.01)? ")))
+
